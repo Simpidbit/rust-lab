0920f553011050def3fd7e1cbab95604dc6d18f0
Simpidbit Isaiah <simpidbit@gmail.com>
2026-02-10 07:02:24 +0800

    完善针对数字输入的异常处理

--- 002.CLCalc/src/main.rs
+++ 002.CLCalc/src/main.rs
@@ -1,29 +1,37 @@
-use std::io;
+use std::io::{self, Write};
 
-fn get_number_from_stdin() -> Result<i64, ()> {
+fn get_number_from_stdin() -> Result<i64, std::io::Error> {
     let mut s = String::new();
-    io::stdin()
-        .read_line(&mut s).unwrap();
-    Ok(s.trim().parse().unwrap())
+    let result: i64;
+
+    loop {
+        print!("Please input an integer (64-bits, signed): ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut s)?;
+        s = s.trim().to_string();
+        match s.parse::<i64>() {
+            Ok(res) => {
+                result = res;
+                break;
+            },
+            Err(_) => {
+                println!("Failed to parse {} to i64.", s);
+                println!("Please try again.");
+                s = "".to_string();
+            }
+        }
+    }
+    Ok(result)
 }
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     println!("Please make a choice:");
     println!("1. Plus");
     println!("2. Multiple");
 
-    let choice: i64 = get_number_from_stdin().unwrap();
-
-    let mut s = String::new();
-    s = "".to_string();
-    io::stdin()
-        .read_line(&mut s).unwrap();
-    let x: i64 = s.trim().parse().unwrap();
-
-    s = "".to_string();
-    io::stdin()
-        .read_line(&mut s).unwrap();
-    let y: i64 = s.trim().parse().unwrap();
+    let choice: i64 = get_number_from_stdin()?;
+    let x: i64 = get_number_from_stdin()?;
+    let y: i64 = get_number_from_stdin()?;
 
     let mut result: i64 = 0;
     if choice == 1 {
@@ -34,4 +42,5 @@ fn main() {
     }
 
     println!("Result is {}", result);
+    Ok(())
 }
