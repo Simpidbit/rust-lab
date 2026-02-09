035cb72338d7087d02f5f8bb8e8e0feacac52381
Simpidbit Isaiah <simpidbit@gmail.com>
2026-02-10 06:32:44 +0800

    尝试Result<T, E>写法，加入get_number_from_stdin()

--- 002.CLCalc/src/main.rs
+++ 002.CLCalc/src/main.rs
@@ -1,16 +1,20 @@
 use std::io;
 
+fn get_number_from_stdin() -> Result<i64, ()> {
+    let mut s = String::new();
+    io::stdin()
+        .read_line(&mut s).unwrap();
+    Ok(s.trim().parse().unwrap())
+}
+
 fn main() {
     println!("Please make a choice:");
     println!("1. Plus");
     println!("2. Multiple");
 
-    let mut s = String::new();
-    io::stdin()
-        .read_line(&mut s).unwrap();
-    
-    let choice: i64 = s.trim().parse().unwrap();
+    let choice: i64 = get_number_from_stdin().unwrap();
 
+    let mut s = String::new();
     s = "".to_string();
     io::stdin()
         .read_line(&mut s).unwrap();
